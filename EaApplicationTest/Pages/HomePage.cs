09aad49bcd927fa251af947b7c62b15e85e84e1d
Dimitar-Stojanovski <dimitar.stojanovski77@gmail.com>
2023-06-03 19:07:49 +0200

    Added product pages and assestions on the name product

---
+++ EaApplicationTest/Pages/HomePage.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaApplicationTest.Pages
+{
+    public class HomePage
+    {
+        private readonly IPage page;
+
+        private ILocator _registerLink => page.GetByRole(AriaRole.Link, new() { Name = "Register" });
+        private ILocator _productLink => page.GetByRole(AriaRole.Link, new() { Name = "Product" });
+
+
+        public HomePage(IPage page)
+        {
+            this.page = page;
+        }
+
+        public async Task ClickRegisterLink()=> await _registerLink.ClickAsync();
+        public async Task ClickOnProductLink()=> await _productLink.ClickAsync();
+        
+        
+    }
+}
---
+++ EaApplicationTest/Pages/ProductPage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaApplicationTest.Pages
+{
+    public class ProductPage
+    {
+        private readonly IPage page;
+        public ProductPage(IPage page)
+        {
+            this.page = page;
+        }
+
+        private ILocator createLink => page.GetByRole(AriaRole.Link, new() { Name = "Create" });
+        private ILocator _nameField => page.GetByLabel("Name");
+        private ILocator _descriptionField => page.GetByLabel("Description");
+        private ILocator _priceField => page.Locator("#Price");
+        private ILocator _selectProduct => page.GetByRole(AriaRole.Combobox, new() { Name = "ProductType" });
+        private ILocator _createBtn => page.GetByRole(AriaRole.Button, new() { Name = "Create" });
+
+
+        public async Task CreateProduct(string name, string description, decimal price,string productType)
+        {
+            await _nameField.FillAsync(name);
+            await _descriptionField.FillAsync(description);
+            await _priceField.FillAsync(price.ToString());
+            await _selectProduct.SelectOptionAsync(productType);
+        }
+
+        public async Task ClickCreateBtn()=> await _createBtn.ClickAsync();
+
+        
+    }
+}
